8ae793106db0a6f596e36d1ca5bd6cdfcbbb7d45
digirik <erikerbar@web.de>
2021-09-15 22:28:24 +0200

    Generate api client from spec file

---
+++ src/generated/apis/index.ts
@@ -0,0 +1,4 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthenticationApi';
+export * from './ListeningSessionApi';
---
+++ src/generated/models/index.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthenticatedRequest';
+export * from './ErrorResponse';
+export * from './FullListeningSession';
+export * from './IsSessionAuthenticatedResponse';
+export * from './NewAuthenticationSessionResponse';
+export * from './NewListeningSessionRequest';
+export * from './PlaylistMetadata';
+export * from './SimpleListeningSession';
+export * from './SimpleUser';
