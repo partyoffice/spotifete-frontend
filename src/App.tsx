d7df1c4f81a1f7fadc92219075738e6312d938bd
digirik <erikerbar@web.de>
2021-09-16 23:29:24 +0200

    Add router and basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React, { FC } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Error404 from './views/Error404';
 
-function App() {
-  const [bla, setBla] = useState<number>(0);
-  useEffect(() => {
-    console.log(bla);
-    setBla(2);
-    bla;
-  }, []);
-  return <div className="bg-red-200">Hello World</div>;
-}
+const App: FC<any> = () => {
+  return (
+    <Router>
+      <div className="h-full w-full flex flex-col">
+        <Navbar></Navbar>
+        <div className="h-full w-full px-2 py-1 bg-gray-800">
+          <Switch>
+            <Route path="/sessions"></Route>
+            <Route path="/account"></Route>
+            <Route exact path="/"></Route>
+            <Route component={Error404}></Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
