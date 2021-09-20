dee7712fe2df5cec1188166a0223e842c1f6abd5
Digirik <erikerbar@web.de>
2021-09-20 20:13:28 +0200

    Wrap components with user provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React, { FC } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import { UserProvider } from './components/UserProvider';
+import Auth from './views/Auth';
 import Error404 from './views/Error404';
 import Home from './views/Home';
 
 const App: FC<any> = () => {
   return (
-    <Router>
-      <div className="h-full w-full flex flex-col">
-        <Navbar></Navbar>
-        <div className="h-full w-full px-2 py-1 bg-gray-800">
-          <Switch>
-            <Route path="/sessions"></Route>
-            <Route path="/account"></Route>
-            <Route exact path="/" component={Home}></Route>
-            <Route component={Error404}></Route>
-          </Switch>
+    <UserProvider>
+      <Router>
+        <div className="h-full w-full flex flex-col">
+          <Navbar></Navbar>
+          <div className="h-full w-full px-2 py-1 bg-gray-800">
+            <Switch>
+              <Route path="/sessions"></Route>
+              <Route path="/account"></Route>
+              <Route path="/auth" component={Auth}></Route>
+              <Route exact path="/" component={Home}></Route>
+              <Route component={Error404}></Route>
+            </Switch>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </UserProvider>
   );
 };
 
