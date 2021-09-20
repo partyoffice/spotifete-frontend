6c8a43908e5b37fa13546d91d95b6153878b2715
Digirik <erikerbar@web.de>
2021-09-20 19:28:30 +0200

    Display "create session" card only when user is logged in

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,17 +1,25 @@
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
 import Card from '../components/Card';
 import Input from '../components/Input';
+import UserContext from '../context/UserContext';
 
 const Home: FC<any> = (props) => {
+  const { userState } = useContext(UserContext);
+
   return (
     <div className="h-full w-full flex">
       <div className="m-auto flex flex-row space-x-4">
         <Card title="Join an existing session">
           <Input placeholder="Session ID" />
         </Card>
-        <Card title="Create a new session">
-          <Input placeholder="New session name" />
-        </Card>
+
+        {userState.isAuthenticated ? (
+          <Card title="Create a new session">
+            <Input placeholder="New session name" />
+          </Card>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
