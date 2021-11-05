a68fa15401910bc9b257f447608439d4cfb1d5a8
eerbar <e.erbar@evosec.de>
2021-11-05 17:04:01 +0100

    Route user to session on sessionIdInput

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,16 +1,36 @@
-import React, { FC, useContext } from 'react';
+import React, { FC, useCallback, useContext } from 'react';
 import Card from '../components/Card';
 import Input from '../components/Input';
 import UserContext from '../context/UserContext';
+import { useHistory } from 'react-router';
 
 const Home: FC<any> = () => {
   const { userState } = useContext(UserContext);
+  const history = useHistory();
+
+  const joinSession = useCallback(
+    (sessionId: string) => {
+      history.push(`/sessions/${sessionId}`);
+    },
+    [history]
+  );
+
+  const handleJoinSessionInputKeyPress = useCallback(
+    (event) => {
+      if (event.key === 'Enter') {
+        joinSession(event.target.value);
+      }
+    },
+    [joinSession]
+  );
+
+  const createSession = useCallback(() => {}, []);
 
   return (
     <div className="h-full w-full flex">
-      <div className="m-auto flex flex-row space-x-4">
+      <div className="m-auto flex flex-col space-y-5">
         <Card title="Join an existing session">
-          <Input placeholder="Session ID" />
+          <Input placeholder="Session ID" onKeyPress={handleJoinSessionInputKeyPress} />
         </Card>
 
         {userState.isAuthenticated ? (
