bc7838316eab1eada1599704638dd89ad1bf782d
Digirik <erikerbar@web.de>
2021-11-05 18:38:59 +0100

    Load session when opening session view

--- src/views/Session.tsx
+++ src/views/Session.tsx
@@ -1,22 +1,46 @@
-import React, { FC, useCallback, useEffect } from 'react';
-import { useParams } from 'react-router';
+import React, { FC, useCallback, useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router';
 import { FullListeningSession } from '../generated/models';
 import useSpotifeteApi from '../hooks/useSpotifeteApi';
 
+export interface SessionState {
+  session?: FullListeningSession;
+}
+
 const Session: FC<any> = () => {
+  const [state, setState] = useState<SessionState>({ session: undefined });
   const { sessionsApi } = useSpotifeteApi();
+  const history = useHistory();
   const { sessionId } = useParams();
 
-  const getListeningSession = useCallback(async (sessionId: string) => {
-    try {
-      const listeningSession: FullListeningSession = await sessionsApi.getListeningSession({ joinId: sessionId });
-      console.log(listeningSession);
-    } catch (e) {}
-  }, []);
+  const getListeningSession = useCallback(
+    async (sessionId: string) => {
+      try {
+        const session = await sessionsApi.getListeningSession({ joinId: sessionId });
+        setState((prevState) => ({ ...prevState, session: session }));
+      } catch (e) {
+        //TODO: typing for error
+        if (e.status === 404) {
+          history.push('/404');
+        }
+
+        //TODO: handle error
+      }
+    },
+    [setState]
+  );
 
   useEffect(() => {
     getListeningSession(sessionId);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEffect(() => {
+    const sessionName = state.session?.title;
+    if (sessionName) {
+      document.title = `Spotifete - ${sessionName}`;
+    }
+  }, [state]);
 
   return <></>;
 };
