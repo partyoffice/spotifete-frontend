801c79cbb670a43b041a05d6984bd04267e6e43f
eerbar <e.erbar@evosec.de>
2021-11-05 17:03:09 +0100

    Extend DetailedHTMLProps in own Input Component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,16 +1,14 @@
 import React, { FC } from 'react';
 
-export interface InputProps {
-  placeholder: string;
-}
+export type InputProps = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
 
 const Input: FC<InputProps> = (props) => {
   return (
     <div className="mb-3 pt-0">
       <input
         type="text"
-        placeholder={props.placeholder}
         className="px-3 py-3 placeholder-blue-grey-300 text-blueGray-600 relative bg-white bg-gray-800 rounded text-sm border-0 shadow outline-none focus:outline-none  focus:ring focus:ring-green-600 w-full"
+        {...props}
       />
     </div>
   );
