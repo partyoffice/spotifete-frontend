e02dbba456c1a5d7e6019ca15b19eb3a276783bc
digirik <erikerbar@web.de>
2021-12-20 22:14:34 +0100

    Make title for card component optional

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,17 @@
 import React, { FC, ReactNode } from 'react';
 
 export interface CardProps {
-  title: string;
+  title: string | undefined;
   children: ReactNode | ReactNode[];
 }
 
 const Card: FC<CardProps> = (props) => {
+  const { title, children } = props;
   return (
     <div className="max-w-sm rounded bg-gray-900 overflow-hidden shadow-lg text-white">
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{props.title}</div>
-        <div className="flex flex-col">{props.children}</div>
+        {title ? <div className="font-bold text-xl mb-2">{title}</div> : <></>}
+        <div className="flex flex-col">{children}</div>
       </div>
     </div>
   );
