dafabe59f9cb202b1941f314f14b3d63acafa4d2
Simon Ermler <simon_ermler@web.de>
2022-01-13 17:55:15 +0100

    Setup storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,16 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:react/jsx-runtime',
-    'prettier',
-  ],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/eslint-recommended", "plugin:@typescript-eslint/recommended", "plugin:react/recommended", "plugin:react-hooks/recommended", "plugin:react/jsx-runtime", "prettier", "plugin:storybook/recommended"],
   ignorePatterns: ['.eslintrc.js', 'prettier.config.js', 'postcss.config.js', 'tailwind.config.js'],
   rules: {
-    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
-    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
-    'no-case-declarations': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn',
+    // Checks effect dependencies
+    'no-case-declarations': 'off'
   },
   env: {
-    browser: true,
-  },
+    browser: true
+  }
 };
