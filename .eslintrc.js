ed4724eaac84f158002139f9622bf49aecd1366d
Simon Ermler <simon_ermler@web.de>
2022-01-14 15:08:18 +0100

    Fix formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,25 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/eslint-recommended", "plugin:@typescript-eslint/recommended", "plugin:react/recommended", "plugin:react-hooks/recommended", "plugin:react/jsx-runtime", "prettier", "plugin:storybook/recommended"],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier',
+    'plugin:storybook/recommended',
+  ],
   ignorePatterns: ['.eslintrc.js', 'prettier.config.js', 'postcss.config.js', 'tailwind.config.js'],
   rules: {
     'react-hooks/rules-of-hooks': 'error',
     // Checks rules of Hooks
     'react-hooks/exhaustive-deps': 'warn',
     // Checks effect dependencies
-    'no-case-declarations': 'off'
+    'no-case-declarations': 'off',
   },
   env: {
-    browser: true
-  }
+    browser: true,
+  },
 };
