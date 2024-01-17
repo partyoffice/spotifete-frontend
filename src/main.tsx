5880aa2871b3a39a58fad0da64b7772f02fc613f
Digirik <info@digirik.de>
2024-01-17 22:14:24 +0100

    Use new render function

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { UserProvider } from './components/UserProvider';
 
-ReactDOM.render(
+const container = document.getElementById('root')!;
+const root = ReactDOMClient.createRoot(container);
+
+root.render(
   <React.StrictMode>
     <UserProvider>
       <App />
     </UserProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
